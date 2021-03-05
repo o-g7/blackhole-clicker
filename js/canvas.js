a910c6c198b077af58e1b79af7cef65a51f7eb1c
galink2077 <gasa12337@gmail.com>
2021-03-05 16:48:51 -0300

    Lógica do jogo montada por completo :D

--- js/canvas.js
+++ js/canvas.js
@@ -1,90 +1,20 @@
 import {corposCelestes} from "./estadoDoJogo.js"
 import Vetor from "./vetor.js"
+import {BuracoNegro} from "./sprite.js"
+import {CorpoCeleste} from "./sprite.js"
 
 const ARESTA_CANVAS = 500
-
-class Sprite {
-    constructor(posicao, largura, altura, imagem,velocidade,angulo=0) {
-        this.posicao = posicao
-        this.largura = largura
-        this.altura = altura
-        this.imagem = imagem
-        this.velocidade = velocidade
-        this.angulo = angulo
-    }
-        
-    desenhando(ctx) {
-        if (this.imagem) {
-            ctx.save()
-            ctx.translate(this.posicao.x,this.posicao.y)
-            ctx.rotate(this.radiano)
-            ctx.drawImage(this.imagem, this.largura/2, this.altura/2, this.largura, this.altura)
-            ctx.restore()
-        }
-    }
-
-    get radiano(){
-        return (this.angulo * 3.14) / 180
-    }
-
-    get centro(){
-        return {
-            x: this.posicao.x + this.largura / 2,
-            y: this.posicao.y + this.altura / 2
-        }
-    }
-
-    horizonteEventos(outraSprite) {
-        let c1 = Math.abs(outraSprite.centro.x - this.centro.x)
-        let c2 = Math.abs(outraSprite.centro.y - this.centro.y)
-        let h = Math.sqrt(c1 ** 2 + c2 ** 2)
-        let r1 = this.altura / 2
-        let r2 = outraSprite.altura / 2
-
-        return h <= r1 + r2
-    }
-}
-
-class BuracoNegro extends Sprite{
-    constructor(posicao,largura,altura,imagem,massa=1){
-        super(posicao,largura,altura,imagem)
-        this.massa = massa
-    }
-}
-
-class CorpoCeleste extends Sprite{
-    constructor(posicao,largura,altura,imagem,velocidade,angulo,buracoNegro){
-        super(posicao,largura,altura,imagem,velocidade,angulo)
-        this.buracoNegro = buracoNegro
-    }
-
-    atualizaCorposCelestes() {
-        this.angulo += this.velocidade
-        if(this.angulo>=360){
-            this.angulo=1
-        }
-        this.posicao = this.posicao.soma(this.velocidade)
-
-    
-        let forcaGravitacional = new Vetor(this.buracoNegro.centro.x,this.buracoNegro.centro.y).subtrai(this.posicao).unitario().multiplica(this.buracoNegro.massa)
-        
-
-        this.velocidade = this.velocidade.soma(forcaGravitacional)
-        
-    }
-}
-
 
 let canvas = document.querySelector('#clicker')
 let ctx = canvas.getContext('2d')
 let massaAtual = 1
-let pontoDeSuccao = new Vetor((ARESTA_CANVAS - massaAtual)/4,(ARESTA_CANVAS - massaAtual)/4)
+let pontoDeSuccao = new Vetor((ARESTA_CANVAS - massaAtual)/3,(ARESTA_CANVAS - massaAtual)/2.5)
 let buracoNegroImg = new Image
 buracoNegroImg.src = "imgs/blackhole-128.png"
 const buracoNegroEl = new BuracoNegro(pontoDeSuccao,128,128,buracoNegroImg,massaAtual)
 let imagemTeste = new Image
 imagemTeste.src = "imgs/blackhole-32.png"
-corposCelestes.push(new CorpoCeleste(new Vetor(-30,-30),32,32,imagemTeste,new Vetor(10,0),0.2,buracoNegroEl))
+corposCelestes.push(new CorpoCeleste(new Vetor(-30,-30),32,32,imagemTeste,new Vetor(1,0),buracoNegroEl))
 
 buracoNegroImg.addEventListener('load',()=>{
     desenhaCanvas()
@@ -104,9 +34,19 @@ function atualizaJogo(){
     }
 }
 
+function engoleCorpos(){
+    for (let corpo of corposCelestes) {
+        const atingiuBuracoNegro = corpo.horizonteEventos(buracoNegroEl)
+        if (atingiuBuracoNegro) {
+            corpo.morrer(new Vetor(-30,-30),new Vetor(1,0)) 
+       }
+    }
+}
+
 function ojogo(){
     desenhaCanvas()
     atualizaJogo()
+    engoleCorpos()
 }
 
 setInterval(()=>{
---
+++ js/sprite.js
@@ -0,0 +1,63 @@
+import Vetor from "./vetor.js"
+
+export class Sprite {
+    constructor(posicao, largura, altura, imagem,velocidade) {
+        this.posicao = posicao
+        this.largura = largura
+        this.altura = altura
+        this.imagem = imagem
+        this.velocidade = velocidade
+    }
+        
+    desenhando(ctx) {
+        if (this.imagem) {
+            ctx.drawImage(this.imagem, this.posicao.x, this.posicao.y, this.largura, this.altura)
+        }
+    }
+
+    get centro(){
+        return {
+            x: this.posicao.x + this.largura / 2,
+            y: this.posicao.y + this.altura / 2
+        }
+    }
+
+    horizonteEventos(outraSprite) {
+        let c1 = Math.abs(outraSprite.centro.x - this.centro.x)
+        let c2 = Math.abs(outraSprite.centro.y - this.centro.y)
+        let h = Math.sqrt(c1 ** 2 + c2 ** 2)
+        let r1 = this.altura / 2
+        let r2 = outraSprite.altura / 2
+
+        return h <= r1 + r2
+    }
+}
+
+export class BuracoNegro extends Sprite{
+    constructor(posicao,largura,altura,imagem,massa=1){
+        super(posicao,largura,altura,imagem)
+        this.massa = massa
+    }
+}
+
+export class CorpoCeleste extends Sprite{
+    constructor(posicao,largura,altura,imagem,velocidade,buracoNegro){
+        super(posicao,largura,altura,imagem,velocidade)
+        this.buracoNegro = buracoNegro
+    }
+
+    atualizaCorposCelestes() {
+        this.posicao = this.posicao.soma(this.velocidade)
+        let forcaGravitacional = new Vetor(this.buracoNegro.centro.x,this.buracoNegro.centro.y).subtrai(this.posicao).unitario().multiplica(this.buracoNegro.massa)
+        this.velocidade = this.velocidade.soma(forcaGravitacional)
+        
+    }
+
+    morrer(posicaoInicial,velocidadeInicial){
+        this.posicao.x = posicaoInicial.x
+        this.posicao.y = posicaoInicial.y
+        this.velocidade.x = velocidadeInicial.x
+        this.velocidade.y = velocidadeInicial.y
+    }
+}
+
